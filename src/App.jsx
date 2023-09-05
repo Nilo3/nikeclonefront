38678ebcd6ff15e9543f154cb17e2c4786ea1a2d
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-09-04 21:27:45 -0300

    work done

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import CustomerReviews from './sections/CustomerReviews';
+import Footer from './sections/Footer';
+import Hero from './sections/Hero';
+import PopularProducts from './sections/PopularProducts';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import SuperQuality from './sections/SuperQuality';
+import Suscribe from './sections/Suscribe';
+import Nav from './components/nav';
+
+const App = () => {
+	return (
+		<main className='relative'>
+			<Nav />
+			<section className='xl:padding-l wide:padding-r padding-b'>
+				<Hero />
+			</section>
+			<section className='padding'>
+				<PopularProducts />
+			</section>
+			<section className='padding'>
+				<SuperQuality />
+			</section>
+			<section className='padding-x py-10'>
+				<Services />
+			</section>
+			<section className='padding'>
+				<SpecialOffer />
+			</section>
+			<section className='bg-pale-blue padding'>
+				<CustomerReviews />
+			</section>
+			<section className='padding-x sm:py-32 py-16 w-full'>
+				<Suscribe />
+			</section>
+			<section className=' bg-black padding-x padding-t pb-8'>
+				<Footer />
+			</section>
+		</main>
+	);
+};
+
+export default App;
